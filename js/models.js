a56e2879d2fac5b46a239b4e2580d3dea6060d26
Achette <i.achette@live.com>
2022-05-16 16:37:18 -0300

    Fixed model.js

--- js/models.js
+++ js/models.js
@@ -2,9 +2,9 @@ let modelsJson = [
     {id:1, name:'T-shirt Black', img:'img/t-shirt-black.png', price:[150.00], sizes:['S', 'M', 'L'], description:'The basic and confortable black t-shirt that goes with everything'},
     {id:2, name:'T-shirt Blue', img:'img/t-shirt-blue.png', price:[100.00], sizes:['S', 'M', 'L'], description:'Stylish blue t-shirt, perfect for any occasion'},
     {id:3, name:'T-shirt Green', img:'img/t-shirt-green.png', price:[100.00], sizes:['S', 'M', 'L'], description:'Basic green t-shirt, elegant and versatile for everyday life'},
-    {id:4, name:'T-shirt Orange', img:'img/t-shirt-orange.png', price:[120.00], sizes:['S', 'M', 'L'], description:'Famoso caça da Messerchmnitt, voado pelos maiores ases da Luftwaffe, a versão G foi a mais produzida'},
-    {id:5, name:'T-shirt Purple', img:'img/t-shirt-purple.png', price:[150.00], sizes:['S', 'M', 'L'], description:'Caça bombardeiro de fabricação americana utilizado pela FAB'},
-    {id:6, name:'T-shirt Red', img:'img/t-shirt-red.png', price:[150.00], sizes:['S', 'M', 'L'], description:'O mais conhecido caça americano, importante escolta para os bombardeiros diurnos'},
-    {id:7, name:'Sweatshirt Orange', img:'img/t-shirt-orange2.png', price:[200.00], sizes:['S', 'M', 'L'], description:'O mais famosa caça britânico, utilizado em toda a guerra'},
-    {id:8, name:'T-shirt Black Yellow', img:'img/t-shirt-yellow.png', price:[100.00], sizes:['S', 'M', 'L'], description:'O mais famoso caça da marinha japonesa, terror do pacífico nos anos iníciais da guerra'}
+    {id:4, name:'T-shirt Orange', img:'img/t-shirt-orange.png', price:[120.00], sizes:['S', 'M', 'L'], description:'Comfortable and elegant, made of 100% cotton'},
+    {id:5, name:'T-shirt Purple', img:'img/t-shirt-purple.png', price:[150.00], sizes:['S', 'M', 'L'], description:'Perfect for your party'},
+    {id:6, name:'T-shirt Red', img:'img/t-shirt-red.png', price:[150.00], sizes:['S', 'M', 'L'], description:'Sophisticated and casual'},
+    {id:7, name:'Sweatshirt Orange', img:'img/t-shirt-orange2.png', price:[200.00], sizes:['S', 'M', 'L'], description:'Comfortable and elegant, made of 100% cotton'},
+    {id:8, name:'T-shirt Yellow', img:'img/t-shirt-yellow.png', price:[100.00], sizes:['S', 'M', 'L'], description:'Ideal for everyday use'}
 ];
